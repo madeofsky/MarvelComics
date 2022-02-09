8f24446700fd6297212ed64a499ba64de88b7f73
madeofsky <blslucasfj1@hotmail.com>
2022-02-09 17:04:36 -0300

    Creating project

---
+++ app/src/main/java/com/lucas/marvelheroesactivity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.lucas.marvelheroesactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lucas.marvelheroesactivity.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
