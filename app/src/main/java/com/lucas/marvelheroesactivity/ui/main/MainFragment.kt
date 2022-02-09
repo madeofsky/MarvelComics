8ce27c2e37b5c47191dd960a27110cc46b42fcc8
madeofsky <blslucasfj1@hotmail.com>
2022-02-09 17:11:53 -0300

    - Deleting comment to be able to push the new branches

--- app/src/main/java/com/lucas/marvelheroesactivity/ui/main/MainFragment.kt
+++ app/src/main/java/com/lucas/marvelheroesactivity/ui/main/MainFragment.kt
@@ -26,7 +26,6 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
